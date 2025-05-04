7529c8e7e5a7197b10c0550033a56b15000fbb2a
Alejandro Ledesma Huayan <143223151+AlejandroLH7@users.noreply.github.com>
2025-05-03 22:36:00 -0500

    Update CajaDeRetiro.java

--- src/cajaderetiro/CajaDeRetiro.java
+++ src/cajaderetiro/CajaDeRetiro.java
@@ -44,6 +44,49 @@ class Usuario {
 
 class Caja {
     
+    private String nombre;
+    private double monto;
+    private final double MIN = 0;
+    private final double MAX = 100000;
+
+    public Caja(String nombre, double montoInicial) {
+        this.nombre = nombre;
+        this.monto = montoInicial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public boolean retirar(double montoARetirar) {
+        if (montoARetirar <= monto && montoARetirar >= MIN) {
+            monto -= montoARetirar;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean añadir(double montoAAñadir) {
+        if (monto + montoAAñadir <= MAX && montoAAñadir > 0) {
+            monto += montoAAñadir;
+            return true;
+        }
+        return false;
+    }
+
+    public void restablecer(double nuevoMonto) {
+        if (nuevoMonto >= MIN && nuevoMonto <= MAX) {
+            monto = nuevoMonto;
+        }
+    }
+
+    public boolean estaVacia() {
+        return monto <= 0;
+    }
 }
 
 class RegistroRetiro {
